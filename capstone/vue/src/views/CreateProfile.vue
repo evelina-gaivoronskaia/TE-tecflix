214faa2dc7fb5c403b773fe69ef04fe5866c26ec
Evelina Gaivoronskaia <evelina.gaivor@gmail.com>
2022-09-27 18:21:50 -0400

    Got the front end and the back end of Creating the Account to work

--- capstone/vue/src/views/CreateProfile.vue
+++ capstone/vue/src/views/CreateProfile.vue
@@ -1,66 +1,94 @@
 <template>
-  <div
-    id="createProfile">
-    <form class="profile-form" @submit.prevent="create">
+  <div id="createProfile">
+    <form class="profile-form" @submit.prevent="createProfile">
       <h1 class="h3 mb-3 font-weight-normal">Create Profile</h1>
       <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstName" placeholder="Your name.." v-model="profile.firstName"/>
+      <input
+        type="text"
+        id="fname"
+        class="form-control"
+        name="firstName"
+        placeholder="Your name.."
+        v-model="profile.firstName"
+      />
 
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastName" placeholder="Your last name.." v-model="profile.lastName"/>
+      <label for="lname">Last Name</label>
+      <input
+        type="text"
+        id="lname"
+        class="form-control"
+        name="lastName"
+        placeholder="Your last name.."
+        v-model="profile.lastName"
+      />
 
-    <label for= "email">Email Address </label>
-    <input type="text" id="email" name="email" placeholder="Your email address" v-model="profile.email"/>
+      <label for="email">Email Address </label>
+      <input
+        type="text"
+        id="email"
+        class="form-control"
+        name="email"
+        placeholder="Your email address"
+        v-model="profile.emailAddress"
+      />
 
-    <select name ="timezone" v-model="profile.timezone">
-      <option value= "Central Daylight Time">Central Daylight Time</option>
-      <option value= "Eastern Daylight Time">Eastern Daylight Time</option>
-      <option value= "Mountain Daylight Time">Mountain Daylight Time</option>
-      <option value= "Mountain Standard Time">Mountain Standard Time</option>
-      <option value= "Pacific Daylight Time">Pacific Daylight Time</option>
-      <option value= "Alaska Daylight Time">Alaska Daylight Time</option>
-      <option value= "Hawaii-Aleutian Standard Time">Hawaii-Aleutian Standard Time</option>
-  </select>
+      <select name="timezone" class="form-control" v-model="profile.timeZone">
+        <option value="CDT">Central Daylight Time</option>
+        <option value="EDT">Eastern Daylight Time</option>
+        <option value="MDT">Mountain Daylight Time</option>
+        <option value="MST">Mountain Standard Time</option>
+        <option value="PDT">Pacific Daylight Time</option>
+        <option value="ADT">Alaska Daylight Time</option>
+        <option value="HST">Hawaii-Aleutian Standard Time</option>
+      </select>
 
-  <input type="checkbox" id="subscribe" name="subscribe" v-model="profile.subscribe"/>
-  <label for= "subscribe">Subscribe to email list </label>
+      <input
+        type="checkbox"
+        id="subscribe"
+        class="form-control"
+        name="subscribe"
+        v-model="profile.subscribedToEmailList"
+      />
+      <label for="subscribedToEmailList">Subscribe to email list </label>
 
-  <input type="submit" value="Submit">
-  </form>
-
-
-</div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
-import Profile from '../services/CreateProfileService';
+import Profile from "../services/CreateProfileService";
 export default {
-name: 'createProfile',
-data(){
-  return{
-    profile:{
-      firstName: "",
-      lastName: "",
-      email:  "",
-      timezone: "",
-      subscribe: false
-
-    }
-  }
-},
-methods:{
-  created(){
-    Profile.createProfile(this.profile).then((response) => {
-      if(response.status == 201) {
-          this.$router.push({
-            name:'home'
-          })
-      }
-    })
-
-  }
-}
-}
+  name: "createProfile",
+  data() {
+    return {
+      profile: {
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        timeZone: "",
+        subscribedToEmailList: false,
+      },
+    };
+  },
+  methods: {
+    createProfile() {
+      Profile.createProfile(this.profile)
+        .then((response) => {
+          if (response.status == 201) {
+            this.$router.push({
+              name: "home",
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -74,7 +102,7 @@ methods:{
   box-sizing: border-box;
 } */
 
-#createProfile{
+#createProfile {
   width: 100%;
   background-color: #0525a8;
   color: orange;
@@ -85,15 +113,16 @@ methods:{
   cursor: pointer;
 }
 
-input[type=submit]:hover {
+input[type="submit"]:hover {
   background-color: white;
 }
 
-.profile-form{
+.profile-form {
   border-radius: 5px;
-  background:linear-gradient(rgba(192, 107, 28, 0.933),rgba(18, 13, 126, 0.933));
+  background: linear-gradient(
+    rgba(192, 107, 28, 0.933),
+    rgba(18, 13, 126, 0.933)
+  );
   padding: 20px;
 }
-
-
 </style>
