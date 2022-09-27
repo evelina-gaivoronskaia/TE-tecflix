5b016c586a5b422be2469dfcec8bf8c2002b1a32
Evelina Gaivoronskaia <evelina.gaivor@gmail.com>
2022-09-27 12:10:38 -0400

    Trying to solve merge conflicts

--- capstone/java/src/main/java/com/techelevator/controller/AccountController.java
+++ capstone/java/src/main/java/com/techelevator/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.AccountDao;
+import com.techelevator.model.Account;
+import com.techelevator.model.AccountAlreadyExistsException;
 import com.techelevator.model.AccountDTO;
+import com.techelevator.model.AccountNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +22,13 @@ public class AccountController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/createProfile", method = RequestMethod.POST)
-    public void createAccount(@Valid @RequestBody AccountDTO newAccount){
-        accountDao.createAccount(newAccount.getUserId(), newAccount.getFirstName(), newAccount.getLastName(), newAccount.getEmailAddress(), newAccount.getTimeZone(), newAccount.getSubscribedToEmailList());
+    public void createAccount(@Valid @RequestBody AccountDTO newAccount) {
+        try {
+            Account account = accountDao.getAccountByEmailAddress(newAccount.getEmailAddress());
+            throw new AccountAlreadyExistsException();
+        } catch (AccountNotFoundException e){
+            accountDao.createAccount(newAccount.getUserId(), newAccount.getFirstName(), newAccount.getLastName(), newAccount.getEmailAddress(), newAccount.getTimeZone(), newAccount.getSubscribedToEmailList());
+        }
     }
 
 }
--- capstone/java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
@@ -1,6 +1,9 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Account;
+import com.techelevator.model.AccountNotFoundException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public class JdbcAccountDao implements AccountDao{
 
@@ -14,5 +17,27 @@ public class JdbcAccountDao implements AccountDao{
     public boolean createAccount(int userId, String firstName, String lastName, String emailAddress, String timeZone, Boolean isSubscribedToEmailList) {
         String insertAccountSql = "INSERT INTO account (user_id, first_name, last_name, email_address, time_zone, email_list) values (?, ?, ?, ?, ?, ?)";
         return jdbcTemplate.update(insertAccountSql, userId, firstName, lastName, emailAddress, timeZone, isSubscribedToEmailList) == 1;
+    }
+
+    @Override
+    public Account getAccountByEmailAddress(String emailAddress) {
+        String sql = "SELECT * FROM account WHERE email_address = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, emailAddress);
+        if(results.next()){
+            return mapRowToAccount(results);
+        } else {
+            throw new AccountNotFoundException();
+        }
+    }
+
+    private Account mapRowToAccount(SqlRowSet rs){
+        Account account = new Account();
+        account.setUserId(rs.getInt("user_id"));
+        account.setFirstName(rs.getString("first_name"));
+        account.setLastName(rs.getString("last_name"));
+        account.setEmailAddress(rs.getString("email_address"));
+        account.setTimeZone(rs.getString("time_zone"));
+        account.setSubscribedToEmailList(rs.getBoolean("email_list"));
+        return account;
     }
 }
