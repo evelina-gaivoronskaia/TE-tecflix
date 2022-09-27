e65db1c3d00f0365e891f338d13c3e6c17bfc131
Terrance White <[Z[Z[Z[Z[Z[Z[white.terranceh@gmail.com>
2022-09-27 08:56:18 -0700

    Created MovieDAO, JDBCMovieDAO,MOVIE model

---
+++ capstone/java/src/main/java/com/techelevator/model/Movie.java
@@ -0,0 +1,69 @@
+package com.techelevator.model;
+
+public class Movie {
+
+    private int movieId;
+    //private String tmdbMovieID;
+    private String genre;
+    private String releaseYear;
+    private String director;
+    private String title;
+    private String summary;
+    private String movieImg;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getMovieImg() {
+        return movieImg;
+    }
+
+    public void setMovieImg(String movieImg) {
+        this.movieImg = movieImg;
+    }
+}
