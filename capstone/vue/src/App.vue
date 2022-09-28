c3ff10981d463da8d65d639ffa041aef8f58bfc3
Evelina Gaivoronskaia <evelina.gaivor@gmail.com>
2022-09-28 12:08:30 -0400

    Edited the Home page in front end

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link id="home" v-bind:to="{ name: 'home' }">Home</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        id="logout"
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
 </template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Monoton&display=swap");
+#nav {
+  display: block;
+  width: 10%;
+  text-align: center;
+  background-color: #010130;
+  max-height: 70px;
+  padding: 5px;
+}
+#home,
+#logout,
+#nav {
+  margin: 5px;
+  color: #e88b65;
+  font-family: "Bebas Neue", sans-serif;
+}
+</style>
--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-    <router-link :to="{name: 'createProfile'}">Create Profile</router-link>
+    <h1 class="header">HOME</h1>
+    <router-link class="link" :to="{ name: 'createProfile' }"
+      >CREATE PROFILE</router-link
+    >
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Monoton&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Monoton&display=swap");
+
+.home {
+  display: block;
+  width: 50%;
+  min-height: 500px;
+  padding: 20px;
+  border: 5px solid #010130;
+  background-color: #010130;
+  margin: 0;
+  text-align: center;
+  position: relative;
+  top: 50%;
+  transform: translateY(30%);
+  left: 25%;
+  right: 25%;
+  color: #ff6626;
+}
+.header {
+  font-size: 150px;
+  font-family: "Monoton", sans-serif;
+  font-weight: 400;
+  margin: 5%;
+  overflow-wrap: break-word;
+  text-size-adjust: auto;
+}
+.link {
+  color: #e88b65;
+  font-size: 50px;
+  font-family: "Bebas Neue", sans-serif;
+  margin: 5%;
+}
+</style>
